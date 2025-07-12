203ac56669e947fe97cb3450a44f9bcb0afba775
Parth Sinha <sinhaparth555@gmail.com>
2025-07-12 08:41:08 +0100

    added source file

---
+++ src/main/java/com/culturalvault/CulturalVaultApplication.java
@@ -0,0 +1,13 @@
+package com.culturalvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/culturalvault/CulturalVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturalvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
