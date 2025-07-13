29825227cb3b632120cdb8b8000c8d7ba25150f6
Parth Sinha <sinhaparth555@gmail.com>
2025-07-13 14:12:38 +0100

    updated security config to add password encoder and api

--- src/main/java/com/culturalvault/config/SecurityConfig.java
+++ src/main/java/com/culturalvault/config/SecurityConfig.java
@@ -8,8 +8,10 @@ package com.culturalvault.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +23,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
  *
  * @author parth
  */
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     /**
      * Password encoder bean - this fixes the missing dependency issue
